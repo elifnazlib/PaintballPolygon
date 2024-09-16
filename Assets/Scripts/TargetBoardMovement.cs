d7b67f3361e00453764fc48d903d1559149bed04
Elif Nazlı Böke <111501335+elifnazlib@users.noreply.github.com>
2024-09-16 19:13:28 +0300

    Target board Spawn (final)

--- Assets/Scripts/TargetBoardMovement.cs
+++ Assets/Scripts/TargetBoardMovement.cs
@@ -1,31 +1,48 @@
 using UnityEngine;
 
+// This script is used to control the movement of the target board.
 public class TargetBoardMovement : MonoBehaviour
 { 
-    private Vector3 endPointPosition;
+    private bool areVariablesAssigned = false;  // Is used to prevent the synchronization problem in TargetBoardSpawnManager.cs (for end point and desired duration)
+
+    public bool AreVariablesAssigned
+    {
+        get {return areVariablesAssigned; }
+        set {areVariablesAssigned = value; }
+    }
+
+    private Vector3 endPointPosition;   // end point position of the target board.
 
     public Vector3 EndPointPosition
     {
         get {return endPointPosition; }
         set {endPointPosition = value; }
     }
-    [SerializeField] private float desiredDuration;
-    private Vector3 startPosition;
-    private float elapsedTime;
+    private float desiredDuration;  // desired duration for the target board movement.
+
+    public float DesiredDuration
+    {
+        get {return desiredDuration; }
+        set {desiredDuration = value; }
+    }
+    private Vector3 startPosition;  // start point position of the target board.
+    private float elapsedTime;  // elapsed time of the Lerp.
 
     void Start()
     {
-        startPosition = transform.position;
+        startPosition = transform.position; // assigning the initial position of target board at the start.
     }
 
     void Update()
     {
-        // Movement logic of the target board
-        elapsedTime += Time.deltaTime;
-        float percentageComplete = elapsedTime / desiredDuration;
+        if(areVariablesAssigned){ // TODO: This line may need to change during object pooling implementation
+            // Movement logic of the target board (https://www.youtube.com/watch?v=MyVY-y_jK1I)
+            elapsedTime += Time.deltaTime;
+            float percentageComplete = elapsedTime / desiredDuration;
 
-        transform.position = Vector3.Lerp(startPosition, endPointPosition, percentageComplete);
+            transform.position = Vector3.Lerp(startPosition, endPointPosition, percentageComplete);
+        }
     }
 
-
+    // TODO: Target boards shall move forward and back 
 }
--- Assets/Scripts/TargetBoardSpawnManager.cs
+++ Assets/Scripts/TargetBoardSpawnManager.cs
@@ -1,34 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Random = System.Random;
 
+// This script controls the creations and randomizations of target boards
+
 public class TargetBoardSpawnManager : MonoBehaviour
 {
-    [SerializeField] private GameObject targetBoardPrefab;
-    [SerializeField] private GameObject[] startPoints;
-    [SerializeField] private GameObject[] endPoints;
-    private int maxSpeed = 10, minSpeed = 0;
-    private Random random;
+    [SerializeField] private GameObject targetBoardPrefab; // Prefab of the target board
+    [SerializeField] private GameObject[] startPoints; // Possible start points of target board
+    [SerializeField] private GameObject[] endPoints; // Possible end points of target board
+    [SerializeField] private float maxDurationForLerp = 10f, minDurationForLerp = 1f; // Minimum and maximum desired duration for lerp operation of target board
+    [SerializeField] private float minWaitDurationForCreations = 1f, maxWaitDurationForCreations = 6f; // Minimum and maximum durations between target boards' creations
+    private Random random; // Random instance from System.Random
 
     void Start()
     {
-        random = new Random();
-        SpawnTargetBoard();
+        random = new Random(); // Initialization of Random instance
+        StartCoroutine(SpawnTargetBoard()); // Starting the coroutine SpawnTargetBoard()
     }
 
-    public void SpawnTargetBoard(){
-        int randomStartPointIndex  = random.Next(0, startPoints.Length);
-        int randomEndPointIndex  = random.Next(0, endPoints.Length);
+    // This method randomizes the variables according to min-max values and instantiates the target boards 
+    IEnumerator SpawnTargetBoard(){
+        int randomStartPointIndex  = random.Next(0, startPoints.Length); // Index of random start point
+        int randomEndPointIndex  = random.Next(0, endPoints.Length); // Index of random end point
+        float randomDurationForLerp = UnityEngine.Random.Range(minDurationForLerp, maxDurationForLerp); // Random duration for lerp (Used UnityEngine.Random.Range() to generate random floats)
+        float randomDurationForCreation = UnityEngine.Random.Range(minWaitDurationForCreations, maxWaitDurationForCreations); // Random duration for creation (Used UnityEngine.Random.Range() to generate random floats)
 
-        GameObject createdTargetBoard = Instantiate(targetBoardPrefab, startPoints[randomStartPointIndex].transform.position, Quaternion.identity);
+        // Instatiation of the target board with assigned prefab, random start point, and rotation of prefab
+        GameObject createdTargetBoard = Instantiate(targetBoardPrefab, startPoints[randomStartPointIndex].transform.position, targetBoardPrefab.transform.rotation);
 
-        createdTargetBoard.GetComponent<TargetBoardMovement>().EndPointPosition = endPoints[randomEndPointIndex].transform.position;
+        TargetBoardMovement createdTargetBoardsTBM = createdTargetBoard.GetComponent<TargetBoardMovement>(); // Storing the TargetBoardMovement script of created target board to use it later (for better performance)
+        createdTargetBoardsTBM.EndPointPosition = endPoints[randomEndPointIndex].transform.position; // Setting the end point variable of the TargetBoardMovement.cs
+        createdTargetBoardsTBM.DesiredDuration = randomDurationForLerp; // Assigning the random speed of the target board
+        createdTargetBoardsTBM.AreVariablesAssigned = true; // To prevent the case "TargetBoardMovement.cs can reach the Lerp method before the created target board's end point position is assigned"
 
-        // TODO: Assign the random speed for the target board
-        // TODO: TargetBoardMovement.cs can reach the Lerp method before the created target board's end point position is assigned
+        Debug.Log($"Waiting for {randomDurationForCreation} seconds."); // Debugging
+        
+        yield return new WaitForSeconds(randomDurationForCreation); // Waiting for some time before creating the other target board
+        StartCoroutine(SpawnTargetBoard()); // Continuing the creation loop
+
         // TODO: If the target board got shot; fall down, stop scoring, wait a while, and destroy
         // TODO: If the target board does not get shot; ???
         // TODO: Object Pooling for optimization
+        // TODO: Set the optimal min max values for randomizations
     }
 }
