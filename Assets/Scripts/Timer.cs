eae66dfcf1331b241f065a3260277802d24d60f3
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2024-09-17 15:22:08 +0300

    FIX for Negative timer Values & minute format

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,15 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private float targetTime = 60.0f; // Target time for the timer
-    [SerializeField] private TextMeshProUGUI timerText;
-    private bool isTimerEnded = false;
+    [SerializeField] private TextMeshProUGUI timerText; // TextMeshProUGUI instance to show the timer on the screen
+    private bool isTimerEnded = false; // Is used to check if the timer is ended
     
     void Update()
     {
-        if (!isTimerEnded)
+        if (!isTimerEnded) // If the timer is not ended
         {
             targetTime -= Time.deltaTime; // Decrease the target time by the time passed since the last frame
-            Debug.Log((int)targetTime); // Debugging
-            int timerTextInteger = (int) targetTime;
-            timerText.text = timerTextInteger.ToString();
+            timerText.text = TimeSpan.FromSeconds(targetTime).ToString(@"mm\:ss"); // Update the timer text on the screen
         }
 
         if (targetTime <= 0.0f) // If the target time is less than or equal to 0
@@ -28,7 +27,7 @@ public class Timer : MonoBehaviour
     
     void TimerEnded()
     {
-        isTimerEnded = true;
+        isTimerEnded = true; // Set the timer as ended
         Debug.Log("Timer has ended!"); // Debugging
     }
 }
