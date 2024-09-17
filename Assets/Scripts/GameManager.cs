fba7478d03c737247a3931c67237bb1b95c49a3e
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2024-09-17 14:41:53 +0300

    score Text on Ui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 // This script is used to control the game logic.
@@ -5,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private int innerScore1, innerScore2, innerScore3, innerScore4, innerScore5; // Scores for inner circles
     [SerializeField] private int score = 0; // Total score of the player
+    [SerializeField] private TextMeshProUGUI scoreText; // TextMeshProUGUI instance to show the score on the screen
 
     public void UpdateScore(string raycastedGameObject) { // This method updates the score according to the raycasted object
         switch (raycastedGameObject)
@@ -27,5 +29,7 @@ public class GameManager : MonoBehaviour
             default:
                 break;
         }
+
+        scoreText.text = score.ToString(); // Updating the score text on the screen
     }
 }
